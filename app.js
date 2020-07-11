21fe400025dbb572aadbcdbb84d44999e10cb544
Adrii <adrivette@MacBook-Pro-de-Adrivette-2.local>
2020-07-11 01:45:51 -0500

    Se arregló un problema en los estilos de tipo

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ var app = express();
 // favicon
 var favicon = require('serve-favicon');
 var path = require('path');
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
